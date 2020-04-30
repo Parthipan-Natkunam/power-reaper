519cc532b531fbe2804e3df2f7713a255078fce6
Parthipan N <natkunamparthipan@gmail.com>
2020-04-30 21:56:05 +0530

    initial commit with barebone code for the scrapper

---
+++ powerreaper/reap.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class power_data:
+    def __init__(self, cols):
+        self.division = cols[0]
+        self.sub_division = cols[1]
+        self.sub_station = cols[2]
+        self.from_time = cols[3]
+        self.to_time = cols[4]
+        self.areas = cols[5]
+
+
+def main():
+    data = []
+    maxIndex = 11
+    excludedIndices = [0, 1, 7, 8, 10, 11]
+    URL = 'https://www.bescom.org/upo/public.php'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    rows = soup.find_all('tr')
+
+    for row in rows:
+        cols = row.find_all('td')
+        colData = []
+        for index, val in enumerate(cols):
+            if (not index in excludedIndices):
+                colData.append(val.text.strip())
+            if(index == maxIndex):
+                data.append(power_data(colData))
+
+    for datum in data:
+        print(datum.division, datum.sub_division, datum.sub_station,
+              datum.from_time, datum.to_time, datum.areas, sep=" --***-- ")
